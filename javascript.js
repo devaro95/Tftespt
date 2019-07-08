dc173da2a73130e65a735a4ad615f5a1d6b0b2bc
varo95m <koke.sohe@gmail.com>
2019-07-08 17:45:27 +0200

    1

--- javascript.js
+++ javascript.js
@@ -6,3 +6,7 @@ function ocultarCaja(arg){
     var caja = document.getElementById("caja" + arg);
     caja.style.visibility = "hidden";
 }
+function select(arg){
+    var seleccionados = document.getElementById("seleccionados");
+    seleccionados.innerHTML = "<img src='Images/Campeones/" + arg + ".png' style='width: 50px'>";
+}
