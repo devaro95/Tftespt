5f7d104e440ea7a946e27bcc733058e461bdff5e
varo95m <koke.sohe@gmail.com>
2019-07-10 06:32:34 +0200

    Terminado constructor de equipos Clases. Añadida noticia.

--- javascript.js
+++ javascript.js
@@ -1,58 +1,371 @@
 
 var asesino = 0;
 var espadachin = 0;
-var arrayAsesinos = ["Evelyn", "Akali", "Katarina", "Kha'zi", "Pyke", "Rengar", "Zed"];
+var luchador = 0;
+var cazador = 0;
+var elem = 0;
+var cam = 0;
+var caballero = 0;
+var hechicero = 0;
+var protector = 0;
+var pistolero = 0;
+var arrayAsesinos = ["Evelynn", "Akali", "Katarina", "Khazix", "Pyke", "Rengar", "Zed"];
 var arrayEspadachin = ["Aatrox", "Draven", "Fiora", "Gangplank", "Shen", "Yasuo"];
+var arrayLuchador = ["RekSai", "Warwick", "Volibear", "Chogath", "Blitzcrank"];
+var arrayCazador = ["Ashe", "Kindred", "Varus", "Vayne"];
+var arrayElem = ["Anivia", "Brand", "Kennen", "Lissandra"];
+var arrayCam = ["Elise", "Gnar", "Nidalee", "Shyvana", "Swain"];
+var arrayCaballero = ["Darius", "Garen", "Mordekaiser", "Poppy", "Kayle", "Sejuani"];
+var arrayHechiero = ["Ahri", "AurelionSol", "Karthus", "Kassadin", "Lulu", "Morgana", "Veigar"];
+var arrayProtector = ["Leona", "Braum"];
+var arrayPistolero = ["Gangplank", "Graves", "Lucian", "MissFortune", "Tristana"];
 
 function mostrarCaja(arg) {
     var caja = document.getElementById("caja" + arg);
     caja.style.visibility = "visible";
 }
-function ocultarCaja(arg){
+function ocultarCaja(arg) {
     var caja = document.getElementById("caja" + arg);
     caja.style.visibility = "hidden";
 }
-function select(arg){
+function select(arg) {
     console.log(arg);
     var img = document.getElementById(arg)
+    //Variables para los numeros de cantidad de champs
+    var numAsesino = document.getElementById("numAsesino");
+    var numLuchador = document.getElementById("numLuchador");
+    var numEspadachin = document.getElementById("numEspadachin");
+    var numCazador = document.getElementById("numCazador");
+    var numElem = document.getElementById("numElem");
+    var numCam = document.getElementById("numCam");
+    var numHechicero = document.getElementById("numHechicero");
+    var numCaballero = document.getElementById("numCaballero");
+    var numProtector = document.getElementById("numProtector");
+    var numPistolero = document.getElementById("numPistolero");
     img.style.filter = "grayscale(100%)";
-    img.setAttribute("onclick","deselect('"+ arg +"');");
+    img.setAttribute("onclick", "deselect('" + arg + "');");
     var seleccionados = document.getElementById("seleccionados");
-    seleccionados.innerHTML += "<img class='imagen grande' id=" + arg +"In src='Images/Campeones/" + arg + ".png'>";
-    if(arrayAsesinos.indexOf(arg)!=-1){
+    seleccionados.innerHTML += "<img class='imagen grande' id=" + arg + "In src='Images/Campeones/" + arg + ".png'>";
+    //aumento de contadores
+    if (arrayAsesinos.indexOf(arg) != -1) {
         asesino++;
-    }
-    if(arrayEspadachin.indexOf(arg)!=-1){
+    } if (arrayEspadachin.indexOf(arg) != -1) {
         espadachin++;
+    } if (arrayLuchador.indexOf(arg) != -1) {
+        luchador++;
+    } if (arrayCazador.indexOf(arg) != -1) {
+        cazador++;
+    } if (arrayElem.indexOf(arg) != -1) {
+        elem++;
+    } if (arrayCam.indexOf(arg) != -1) {
+        cam++;
+    } if (arrayCaballero.indexOf(arg) != -1) {
+        caballero++;
+    } if (arrayHechiero.indexOf(arg) != -1) {
+        hechicero++;
+    } if (arrayProtector.indexOf(arg) != -1) {
+        protector++;
+    } if (arrayPistolero.indexOf(arg) != -1) {
+        pistolero++;
     }
-    if(asesino>0){
+    //asesino
+    if (asesino > 0) {
         var clase = document.getElementById("Asesino");
         clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numAsesino.innerHTML = asesino;
+        if (asesino == 3) {
+            var div1 = document.getElementById("Asesino1");
+            div1.style.backgroundColor = "#c9aa71"
+        } else if (asesino == 6) {
+            var div1 = document.getElementById("Asesino2");
+            div1.style.backgroundColor = "#c9aa71"
+        }
     }
-    if(espadachin>0){
+    //luchador
+    if (luchador > 0) {
+        var clase = document.getElementById("Luchador");
+        clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numLuchador.innerHTML = luchador;
+        if (luchador == 2) {
+            var div1 = document.getElementById("Luchador1");
+            div1.style.backgroundColor = "#c9aa71"
+        } else if (luchador == 4) {
+            var div1 = document.getElementById("Luchador2");
+            div1.style.backgroundColor = "#c9aa71"
+        }
+    }
+    //espadachin
+    if (espadachin > 0) {
         var clase = document.getElementById("Espadachin");
         clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numEspadachin.innerHTML = espadachin;
+        if (espadachin == 3) {
+            var div1 = document.getElementById("Espadachin1");
+            div1.style.backgroundColor = "#c9aa71"
+        } else if (espadachin == 6) {
+            var div1 = document.getElementById("Espadachin2");
+            div1.style.backgroundColor = "#c9aa71"
+        }
+    }
+    //cazador
+    if (cazador > 0) {
+        var clase = document.getElementById("Cazador");
+        clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numCazador.innerHTML = cazador;
+        if (cazador == 2) {
+            var div1 = document.getElementById("Cazador1");
+            div1.style.backgroundColor = "#c9aa71"
+        } else if (cazador == 4) {
+            var div1 = document.getElementById("Cazador2");
+            div1.style.backgroundColor = "#c9aa71"
+        }
+    }
+    //elementalista
+    if (elem > 0) {
+        var clase = document.getElementById("Elementalista");
+        clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numElem.innerHTML = elem;
+        if (elem == 3) {
+            var div1 = document.getElementById("Elem1");
+            div1.style.backgroundColor = "#c9aa71"
+        }
+    }
+    //Cambiaformas
+    if (cam > 0) {
+        var clase = document.getElementById("Cambiaformas");
+        clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numCam.innerHTML = cam;
+        if (cam == 3) {
+            var div1 = document.getElementById("Cam1");
+            div1.style.backgroundColor = "#c9aa71"
+        }
+    }
+    //Caballero
+    if (caballero > 0) {
+        var clase = document.getElementById("Caballero");
+        clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        var div1 = document.getElementById("Caballero1");
+        var div2 = document.getElementById("Caballero2");
+        var div3 = document.getElementById("Caballero3");
+        numCaballero.innerHTML = caballero;
+        if (caballero == 2) {
+            div1.style.backgroundColor = "#c9aa71"
+        } else if (caballero == 4) {
+            div2.style.backgroundColor = "#c9aa71"
+        } else if (caballero == 6) {
+            div3.style.backgroundColor = "#c9aa71"
+        }
+    }
+    //Hechicero
+    if (hechicero > 0) {
+        var clase = document.getElementById("Hechicero");
+        clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numHechicero.innerHTML = hechicero;
+        if (hechicero == 3) {
+            var div1 = document.getElementById("Hechicero1");
+            div1.style.backgroundColor = "#c9aa71"
+        } else if (hechicero == 6) {
+            var div1 = document.getElementById("Hechicero2");
+            div1.style.backgroundColor = "#c9aa71"
+        }
+    }
+    //Protector
+    if (protector > 0) {
+        var clase = document.getElementById("Protector");
+        clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numProtector.innerHTML = protector;
+        if (protector == 2) {
+            var div1 = document.getElementById("Protector1");
+            div1.style.backgroundColor = "#c9aa71"
+        }
+    }
+    //Pistolero
+    if (pistolero > 0) {
+        var clase = document.getElementById("Pistolero");
+        clase.classList.remove("byn");
+        clase.classList.add("seleccionado");
+        numPistolero.innerHTML = pistolero;
+        if (pistolero == 2) {
+            var div1 = document.getElementById("Pistolero1");
+            div1.style.backgroundColor = "#c9aa71"
+        } else if (pistolero == 4) {
+            var div1 = document.getElementById("Pistolero2");
+            div1.style.backgroundColor = "#c9aa71"
+        }
     }
 }
-function deselect(arg){
+
+
+function deselect(arg) {
     var img = document.getElementById(arg);
     var seleccionados = document.getElementById("seleccionados");
-    var borrar = document.getElementById(arg +"In");
+    var borrar = document.getElementById(arg + "In");
     seleccionados.removeChild(borrar);
     img.style.removeProperty("filter")
-    img.setAttribute("onclick","select('" + arg +"');");
-    if(arrayAsesinos.indexOf(arg)!=-1){
+    img.setAttribute("onclick", "select('" + arg + "');");
+    if (arrayAsesinos.indexOf(arg) != -1) {
         asesino--;
-    }
-    if(arrayEspadachin.indexOf(arg)!=-1){
+        numAsesino.innerHTML = asesino;
+    } if (arrayEspadachin.indexOf(arg) != -1) {
         espadachin--;
+        numEspadachin.innerHTML = espadachin;
+    } if (arrayLuchador.indexOf(arg) != -1) {
+        luchador--;
+        numLuchador.innerHTML = luchador;
+    } if (arrayCazador.indexOf(arg) != -1) {
+        cazador--;
+        numCazador.innerHTML = cazador;
+    } if (arrayElem.indexOf(arg) != -1) {
+        elem--;
+        numElem.innerHTML = elem;
+    } if (arrayCam.indexOf(arg) != -1) {
+        cam--;
+        numCam.innerHTML = cam;
+    } if (arrayCaballero.indexOf(arg) != -1) {
+        caballero--;
+        numCaballero.innerHTML = caballero;
+    } if (arrayHechiero.indexOf(arg) != -1) {
+        hechicero--;
+        numHechicero.innerHTML = protector;
+    } if (arrayProtector.indexOf(arg) != -1) {
+        protector--;
+        numProtector.innerHTML = protector;
+    } if (arrayPistolero.indexOf(arg) != -1) {
+        pistolero--;
+        numPistolero.innerHTML = pistolero;
     }
-    if(asesino==0){
+
+    if (asesino == 0) {
         var clase = document.getElementById("Asesino");
         clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (asesino > 0) {
+        var div1 = document.getElementById("Asesino1");
+        var div2 = document.getElementById("Asesino2");
+        if (asesino < 3) {
+            div1.removeAttribute("style");
+        } else if (asesino < 6) {
+            div2.removeAttribute("style");
+        }
     }
-    if(espadachin==0){
+    if (luchador == 0) {
+        var clase = document.getElementById("Luchador");
+        clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (luchador > 0) {
+        var div1 = document.getElementById("Luchador1");
+        var div2 = document.getElementById("Luchador2");
+        if (luchador < 2) {
+            div1.removeAttribute("style");
+        } else if (luchador < 4) {
+            div2.removeAttribute("style");
+        }
+    }
+    if (espadachin == 0) {
         var clase = document.getElementById("Espadachin");
         clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (espadachin > 0) {
+        var div1 = document.getElementById("Espadachin1");
+        var div2 = document.getElementById("Espadachin2");
+        if (espadachin < 3) {
+            div1.removeAttribute("style");
+        } else if (espadachin < 6) {
+            div2.removeAttribute("style");
+        }
+    }
+    if (cazador == 0) {
+        var clase = document.getElementById("Cazador");
+        clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (cazador > 0) {
+        var div1 = document.getElementById("Cazador1");
+        var div2 = document.getElementById("Cazador2");
+        if (cazador < 2) {
+            div1.removeAttribute("style");
+        } else if (cazador < 4) {
+            div2.removeAttribute("style");
+        }
+    }
+    if (elem == 0) {
+        var clase = document.getElementById("Elementalista");
+        clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (elem > 0) {
+        var div1 = document.getElementById("Elem1");
+        if (elem < 3) {
+            div1.removeAttribute("style");
+        }
+    }
+    if (cam == 0) {
+        var clase = document.getElementById("Cambiaformas");
+        clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (cam > 0) {
+        var div1 = document.getElementById("Cam1");
+        if (cam < 3) {
+            div1.removeAttribute("style");
+        }
+    }
+    if (caballero == 0) {
+        var clase = document.getElementById("Caballero");
+        clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (caballero > 0) {
+        var div1 = document.getElementById("Caballero1");
+        var div2 = document.getElementById("Caballero2");
+        var div3 = document.getElementById("Caballero3");
+        if (caballero < 2) {
+            div1.removeAttribute("style");
+        } else if (caballero < 4) {
+            div2.removeAttribute("style");
+        } else if (caballero < 6) {
+            div3.removeAttribute("style");
+        }
+    }
+    if (hechicero == 0) {
+        var clase = document.getElementById("Hechicero");
+        clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (hechicero > 0) {
+        var div1 = document.getElementById("Hechicero1");
+        var div2 = document.getElementById("Hechicero2");
+        if (hechicero < 3) {
+            div1.removeAttribute("style");
+        } else if (hechicero < 6) {
+            div2.removeAttribute("style");
+        }
+    }
+    if (protector == 0) {
+        var clase = document.getElementById("Protector");
+        clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (protector > 0) {
+        var div1 = document.getElementById("Protector1");
+        if (protector < 2) {
+            div1.removeAttribute("style");
+        }
+    }
+    if (pistolero == 0) {
+        var clase = document.getElementById("Pistolero");
+        clase.classList.add("byn");
+        clase.classList.remove("seleccionado");
+    } else if (pistolero > 0) {
+        var div1 = document.getElementById("Pistolero1");
+        var div2 = document.getElementById("Pistolero2");
+        if (pistolero < 2) {
+            div1.removeAttribute("style");
+        }else if(pistolero < 4){
+            div2.removeAttribute("style");
+        }
     }
 }
--- noticias.php
+++ noticias.php
@@ -1,22 +1,20 @@
 <head>
     <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $basededatos = "ftfesp";
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $basededatos);
-        // Check connection
-        $consulta = "Select * from noticias where id = 1";
-        $con = mysqli_query($conn, $consulta);
-        $con = mysqli_fetch_assoc($con);
+
+        $conn = mysqli_connect("PMYSQL110.dns-servicio.com", "varo", "&qdGw743", "6795849_tftesp");
+        mysqli_set_charset($conn,'UTF8');
+        $consulta = 'select * from noticias where slug = "'.$_REQUEST['s'].'"';
+        $resultado = $conn->query($consulta);
+        $datos = $resultado->fetch_assoc();
+
     ?>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" type="text/css" href="parchestyle.css">
+    <link rel="stylesheet" type="text/css" href="noticiasstyle.css">
     <link href="https://fonts.googleapis.com/css?family=Merriweather&display=swap" rel="stylesheet">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <meta charset="utf-8" />
-    <title><?php echo $con['titular']; ?> - TFTEsp</title>
+    <title><?php echo $datos['titular']; ?> - TFTEsp</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </head>
 
@@ -27,7 +25,7 @@
                 <div id="leftbar"><img src="Images/newlogo.png" style="width:110px"></div>
             </a>
             <div id="midbar">
-            <a class="navbarlink" href="index.html">INICIO</a>
+                <a class="navbarlink" href="index.html">INICIO</a>
                 <a class="selected" href="campeones.html">CAMPEONES</a>
                 <a class="navbarlink" href="items.html">OBJETOS</a>
                 <a class="navbarlink" href="clases.html">CLASES Y ORIGENES</a>
@@ -38,7 +36,7 @@
             </a>
         </div>
     </div>
-    <div id="menu">
+    <div id="menuNoticias">
         <div style="background: rgb(18,18,18, 0.75); border: 2px solid #c9aa71;color: #c9aa71">
             <div style="width: 100%;text-align: center">
                 <div style="width:100%">
@@ -46,10 +44,47 @@
                     <img src="tf-soon.jpg" style="width: 100%;border-bottom: solid #c9aa71;">
                 </div>
                 <!--Titulo noticia-->
-                <h1><?php echo $con['titular']; ?></h1>
-                <p>
+                <div id="tituloNoticia">
+                    <?php echo $datos['titular']; ?> </h1>
+                </div>
+                <?php $arr = explode(PHP_EOL,$datos['noticia']);
+                    foreach($arr as $line){
+                        if(!strcmp(substr($line,0,3),'###')){
+                            print(
+                                "<div class='titularnoticias'>
+                                ".substr($line,3)."
+                                </div>"
+                            ) ;
+                        } else {
+                            print("
+                            <div class='descripcionNoticia'>
+                              ".$line."
+                            </div>");
+                        }
+                    }?>
+            </div>
+        </div>
 
-                </p>
+    </div>
+    <div id="footer">
+        <div style="float: left; height:100%; min-width: 415px; ">
+            <div style="min-width: 415px; width: 415px; margin-top:10px; margin-left:10px;"><img
+                    src="Images/newlogo.png" style="width:50%">
+            </div>
+            <p1 align="bottom" style=" margin-left: 20px; ">© 2019 www.tftesp.com</p1>
+        </div>
+        <div style="min-width:200px; width: 50%; height: 100%; float:left; text-align:center;">
+            <div style="width:60%; margin:auto; margin-top: 45px;">
+                Teamfight Tactics ESP se creó según la política "Galimatías legal" de Riot Games usando
+                recursos que
+                son propiedad de Riot Games. Riot Games no respalda ni patrocina este proyecto.
+            </div>
+        </div>
+        <div style="float: left; height:100%; width: 10%; min-width:200px;">
+            <div style="margin-top:45px;">Contacto: tftesp@gmail.com <br>
+                <a href="https://twitter.com/TFT_Esp">
+                    <img src="Images/twitter.png" style="width:20px; padding-top:8px;">
+                </a>
             </div>
         </div>
     </div>
